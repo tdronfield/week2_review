5dc0c03da59a9e9db5da8154ce0c3605fcd2b70e
Dronfield <t_dronfield@ats-hr420-dm26.fc.ca>
2019-09-09 19:47:49 -0400

    adding js data object file

---
+++ js/modules/DataObject.js
@@ -0,0 +1,14 @@
+// objects are just collections of keys and values
+// remember to seperate the keys and values with a comma
+// and that's about it!
+// and use a colon between the key and the value.
+// key = "name", value = "Trevor"
+
+const Person = {
+    name: "Trevor",
+    eyeColor: "blue",
+    hairColor: "red",
+    role: "Professor"
+}
+
+export default Person;
