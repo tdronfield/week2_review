ebd0078074791adf63b5f9f099b9d379320b7ac3
Dronfield <t_dronfield@ats-hr420-dm31.fc.ca>
2019-09-16 19:03:32 -0400

    doing team bio stuff

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
-import Person from "./modules/DataObject.js";
+import People from "./modules/DataObject.js";
 
 
 // IIFE - Immediately Invoked Function Expression
 (() => {
     console.log('fired!')
+    //grab the bio container and get ready to populate its contents
+    const bioInfo = document.querySelector('.bio-wrapper').children;
 
-    console.log(Person);
+    function showProfData() {
+        // change data on the page here
 
-    debugger;
+        // this is the label on the button (Name)
+        let currentProf = this.textContent;
+        
+        //this refers to the heading tag (the first child of the biowrapper div)
+        bioInfo[0].textContent = People[currentProf].name;
+        // this refers to the paragraph tag (the second child of the biowrapper div)
+        bioInfo[1].textContent = People[currentProf].role;
+
+        bioInfo[2].textContent = People[currentProf].bio;
+
+        bioInfo[3].src = `images/${People[currentProf].avatar}`;
+    }
+
+    for (let prof in People) {
+        console.log(prof);
+
+        // create a button for every prof/entry in our object
+        let teamButton = document.createElement('button');
+
+        // set the button label (text) to the prof name (current rntry in object)
+        teamButton.textContent = prof;
+
+        // add some event handling so that when we click one of our new buttons, we can show the right data
+        teamButton.addEventListener('click', showProfData);
+
+        //add the button to the top section on our web page
+        document.querySelector("#section1").appendChild(teamButton);
+    }
+
+    //debugger;
 })();
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -4,11 +4,40 @@
 // and use a colon between the key and the value.
 // key = "name", value = "Trevor"
 
-const Person = {
-    name: "Trevor",
-    eyeColor: "blue",
-    hairColor: "red",
-    role: "Professor"
+const Team = {
+    Trevor : {
+        name: "Trevor",
+        bio: "I don't know where I am... can you please take me home?",
+        role: "Professor",
+        avatar: "olaf.jpeg"
+    },
+
+    Joe : {
+        name: "Joe",
+        bio: "My name Joe",
+        role: "Professor",
+        avatar: "pooh.jpeg"
+    },
+    
+    Jarrod : {
+        name: "Jarrod",
+        bio: "I always carry my water bottle",
+        role: "Professor",
+        avatar: "zoolander.jpg"
+    },
+
+    Justin : {
+        name: "Justin",
+        bio: "I'm in charge",
+        role: "Coordinator",
+        avatar: "justin.jpeg"
+    },
+
+
+
+    
+
+
 }
 
-export default Person;
+export default Team;
